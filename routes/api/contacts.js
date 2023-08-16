1c04f1873e15096dab406f5ababa0da8d7d29410
Kamila Adamska <121kama121@gmail.com>
2023-08-16 02:59:29 +0200

    add validation

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { nanoid } = require("nanoid");
 const router = express.Router();
+const Joi = require("joi");
 
 const {
   listContacts,
@@ -39,19 +40,27 @@ router.get("/:id", async (req, res, __) => {
 });
 
 router.post("/", async (req, res, __) => {
-  const { name, email, phone } = req.body;
-  if (!name || !email || !phone) {
+  const body = req.body;
+
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(30).required(),
+    email: Joi.string().email().required(),
+    phone: Joi.string().min(7).max(15).required(),
+  });
+
+  const validation = schema.validate(body);
+
+  if (validation.error) {
     return res.status(400).json({
       status: "error",
       code: 400,
-      message: "missing required name, email or phone field",
+      message: `missing required field or there is something wrong in your data: ${validation.error}`,
     });
   }
+
   const newContact = {
     id: nanoid(),
-    name,
-    email,
-    phone,
+    ...body,
   };
   addContact(newContact);
   res.status(201).json({ status: "success", code: 201, data: newContact });
@@ -79,13 +88,25 @@ router.delete("/:id", async (req, res, __) => {
 router.put("/:id", async (req, res, __) => {
   const { id } = req.params;
   const body = req.body;
-  if (!body) {
+
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(30),
+    email: Joi.string().email(),
+    phone: Joi.string().min(7).max(15),
+  }).required();
+
+  const validation = schema.validate(body);
+
+  if (validation.error || Object.keys(body).length === 0) {
     return res.status(400).json({
       status: "error",
       code: 400,
-      message: "missing fields",
+      message: `there is something wrong in your data: ${
+        validation.error ? validation.error : "missing fields"
+      }`,
     });
   }
+
   const contactToUpdate = await getContactById(id);
   if (!contactToUpdate) {
     return res.status(404).json({
@@ -96,6 +117,7 @@ router.put("/:id", async (req, res, __) => {
   }
   const updatedContact = { ...contactToUpdate, ...body };
   updateContact(id, updatedContact);
+  console.log(body);
   res.json({ status: "success", code: 200, data: updatedContact });
 });
 
